b15ce7b1ea6fbe5a9ef84fe3448623de4c07b4f1
bangbang444 <kim1228com@naver.com>
2025-12-29 17:40:26 +0900

    [FEAT] swagger 추가(#3)

---
+++ src/main/java/bangbang/gourmet/config/SwaggerConfig.java
@@ -0,0 +1,25 @@
+package bangbang.gourmet.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .components(new Components())
+                .info(apiInfo());
+    }
+
+    private Info apiInfo() {
+        return new Info()
+                .title("Gourmet API")
+                .description("맛집 사이트 Gourmet Swagger입니다.")
+                .version("1.0");
+    }
+}
