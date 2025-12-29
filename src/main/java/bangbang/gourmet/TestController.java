db6976565837c5c6f5e1c681b625bd6e8685ba60
bangbang444 <kim1228com@naver.com>
2025-12-29 13:54:56 +0900

    [FEAT] Test Controller 생성

--- src/main/java/bangbang/gourmet/TestController.java
+++ src/main/java/bangbang/gourmet/TestController.java
@@ -1,5 +1,8 @@
 package bangbang.gourmet;
 
+import bangbang.gourmet.common.response.ErrorCode;
+import bangbang.gourmet.common.response.Response;
+import bangbang.gourmet.common.response.SuccessCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,8 +10,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestController {
 
-    @GetMapping("/hi")
-    public ResponseEntity<String> test(){
-        return ResponseEntity.accepted().body("ok");
+    @GetMapping("/success")
+    public Response<String> success(){
+        return Response.success(SuccessCode.SUCCESS, "success");
+    }
+
+    @GetMapping("/error")
+    public Response<String> error(){
+        return Response.error(ErrorCode.INTERNAL_SERVER_ERROR, "error");
     }
 }
