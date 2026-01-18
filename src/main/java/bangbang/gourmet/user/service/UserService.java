c74f15af6eb0f59ce79de3e7777ffcfe7f60f38a
bangbang444 <kim1228com@naver.com>
2026-01-19 01:56:18 +0900

    [FEAT] 회원가입 및 로그인

--- src/main/java/bangbang/gourmet/user/service/UserService.java
+++ src/main/java/bangbang/gourmet/user/service/UserService.java
@@ -2,7 +2,11 @@ package bangbang.gourmet.user.service;
 
 import bangbang.gourmet.user.controller.dto.KakaoTokenResponse;
 import bangbang.gourmet.user.controller.dto.KakaoUserInfo;
+import bangbang.gourmet.user.controller.dto.LoginResponse;
+import bangbang.gourmet.user.entity.User;
+import bangbang.gourmet.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
@@ -10,6 +14,12 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.client.WebClient;
 
+import java.util.UUID;
+
+import static bangbang.gourmet.common.domain.Role.*;
+import static bangbang.gourmet.common.domain.SocialProvider.*;
+
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class UserService {
@@ -22,6 +32,8 @@ public class UserService {
 
     @Value("${kakao.client.redirect-uri}")
     private String redirectUri;
+
+    private final UserRepository userRepository;
 
     public KakaoTokenResponse getKakaoToken(String code){
 
@@ -55,5 +67,23 @@ public class UserService {
                 .retrieve()
                 .bodyToMono(KakaoUserInfo.class)
                 .block();
+    }
+
+    public LoginResponse loginOrSignup(KakaoUserInfo userInfo){
+        User user = userRepository.findByEmailAndProvider(userInfo.getEmail(), KAKAO)
+                .orElseGet(() -> {
+                    String tempNickname = UUID.randomUUID().toString().substring(0, 8);
+                    User newUser = User.builder()
+                            .email(userInfo.getEmail())
+                            .nickname(tempNickname)
+                            .role(ROLE_USER)
+                            .providerId(String.valueOf(userInfo.id()))
+                            .provider(KAKAO)
+                            .build();
+                    return userRepository.save(newUser);
+                });
+
+        // TODO: JWT 토큰 생성 로직
+        return LoginResponse.of("token");
     }
 }
