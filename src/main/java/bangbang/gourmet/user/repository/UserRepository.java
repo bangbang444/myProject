27ecb629da802880122949e1e1ed841c6cdff266
bangbang444 <kim1228com@naver.com>
2026-01-19 02:28:22 +0900

    [CHORE] 불필요한 애노테이션 제거

--- src/main/java/bangbang/gourmet/user/repository/UserRepository.java
+++ src/main/java/bangbang/gourmet/user/repository/UserRepository.java
@@ -3,11 +3,9 @@ package bangbang.gourmet.user.repository;
 import bangbang.gourmet.common.domain.SocialProvider;
 import bangbang.gourmet.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
-@Repository
 public interface UserRepository extends JpaRepository<User,Long> {
     Optional<User> findByEmailAndProvider(String email, SocialProvider provider);
 }
