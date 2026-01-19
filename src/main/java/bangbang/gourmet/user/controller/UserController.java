5036fd01602874a258f4bfa782332d535821e9da
bangbang444 <kim1228com@naver.com>
2026-01-20 01:04:28 +0900

    [REFACTOR] 카카오 로그인 로직 구조 개선 및 트랜잭션 최적화

--- src/main/java/bangbang/gourmet/user/controller/UserController.java
+++ src/main/java/bangbang/gourmet/user/controller/UserController.java
@@ -2,13 +2,10 @@ package bangbang.gourmet.user.controller;
 
 import bangbang.gourmet.common.response.Response;
 import bangbang.gourmet.common.response.SuccessCode;
-import bangbang.gourmet.user.controller.dto.KakaoTokenResponse;
-import bangbang.gourmet.user.controller.dto.KakaoUserInfo;
 import bangbang.gourmet.user.controller.dto.LoginResponse;
-import bangbang.gourmet.user.service.UserService;
+import bangbang.gourmet.user.service.UserAuthService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,14 +15,11 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 public class UserController {
 
-    private final UserService userService;
+    private final UserAuthService userService;
 
     // TODO: 프론트엔드 생기면 바꿀 예정
     @GetMapping("/auth/kakao/callback")
-    public Response<LoginResponse> signup(@RequestParam String code){
-        KakaoTokenResponse tokens = userService.getKakaoToken(code);
-        KakaoUserInfo userInfo = userService.getKakaoUser(tokens.accessToken());
-
-        return Response.success(SuccessCode.SUCCESS, userService.loginOrSignup(userInfo));
+    public Response<LoginResponse> kakaoLogin(@RequestParam String code){
+        return Response.success(SuccessCode.SUCCESS, userService.loginWithKakao(code));
     }
 }
--- src/main/java/bangbang/gourmet/user/service/UserService.java
+++ src/main/java/bangbang/gourmet/user/service/UserService.java
@@ -1,19 +1,13 @@
 package bangbang.gourmet.user.service;
 
-import bangbang.gourmet.config.KakaoProperties;
-import bangbang.gourmet.user.controller.dto.KakaoTokenResponse;
 import bangbang.gourmet.user.controller.dto.KakaoUserInfo;
 import bangbang.gourmet.user.controller.dto.LoginResponse;
 import bangbang.gourmet.user.entity.User;
 import bangbang.gourmet.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.reactive.function.BodyInserters;
-import org.springframework.web.reactive.function.client.WebClient;
 
 import java.util.UUID;
 
@@ -24,36 +18,11 @@ import static bangbang.gourmet.common.domain.SocialProvider.*;
 @Service
 @RequiredArgsConstructor
 public class UserService {
-    private final WebClient kakaoAuthClient;
-    private final WebClient kakaoApiClient;
-    private final KakaoProperties kakaoProperties;
+
     private final UserRepository userRepository;
 
-    public KakaoTokenResponse getKakaoToken(String code){
-        // TODO: 동기 - 비동기
-        return kakaoAuthClient.post()
-            .uri("/oauth/token")
-            .body(BodyInserters.fromFormData("grant_type", "authorization_code")
-                    .with("client_id", kakaoProperties.id()) // REST API 키
-                    .with("client_secret", kakaoProperties.secret())
-                    .with("redirect_uri", kakaoProperties.redirectUri())
-                    .with("code", code))
-            .retrieve()
-            .bodyToMono(KakaoTokenResponse.class)
-            .block();
-    }
-
-    public KakaoUserInfo getKakaoUser(String accessToken) {
-        return kakaoApiClient.get()
-                .uri("/v2/user/me")
-                .header(HttpHeaders.AUTHORIZATION, "Bearer " + accessToken)
-                .retrieve()
-                .bodyToMono(KakaoUserInfo.class)
-                .block();
-    }
-
     @Transactional
-    public LoginResponse loginOrSignup(KakaoUserInfo userInfo){
+    public LoginResponse loginOrRegister(KakaoUserInfo userInfo){
         User user = userRepository.findByEmailAndProvider(userInfo.getEmail(), KAKAO)
                 .orElseGet(() -> {
                     String tempNickname = UUID.randomUUID().toString().substring(0, 8);
