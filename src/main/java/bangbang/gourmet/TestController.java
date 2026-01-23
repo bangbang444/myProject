d2f15892515180d081dc449f28fb0d136f12a5f7
bangbang444 <kim1228com@naver.com>
2026-01-24 00:39:13 +0900

    [FEAT] 비동기 크롤링으로 변경

--- src/main/java/bangbang/gourmet/TestController.java
+++ src/main/java/bangbang/gourmet/TestController.java
@@ -3,16 +3,20 @@ package bangbang.gourmet;
 import bangbang.gourmet.common.response.ErrorCode;
 import bangbang.gourmet.common.response.Response;
 import bangbang.gourmet.common.response.SuccessCode;
+import bangbang.gourmet.crawler.CrawlTargetManger;
+import bangbang.gourmet.crawler.NaverCrawlSyncService;
 import bangbang.gourmet.crawler.NaverCrawler;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequiredArgsConstructor
 public class TestController {
 
-    private final NaverCrawler naverCrawler;
+    private final NaverCrawlSyncService naverCrawlSyncService;
 
     @GetMapping("/success")
     public Response<String> success(){
@@ -26,7 +30,7 @@ public class TestController {
 
     @GetMapping("/crawl")
     public Response<String> crawl(){
-        naverCrawler.crawl();
+        naverCrawlSyncService.startAsyncCrawl();
         return Response.success(SuccessCode.SUCCESS, "crawl");
     }
 }
