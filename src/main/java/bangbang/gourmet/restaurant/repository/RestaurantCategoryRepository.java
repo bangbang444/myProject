99592a3ad0ad42e6a8f9370428b4b74bf002962d
bangbang444 <kim1228com@naver.com>
2026-01-24 01:02:40 +0900

    [FEAT] 라이브러리 교체

--- src/main/java/bangbang/gourmet/restaurant/repository/RestaurantCategoryRepository.java
+++ src/main/java/bangbang/gourmet/restaurant/repository/RestaurantCategoryRepository.java
@@ -2,10 +2,10 @@ package bangbang.gourmet.restaurant.repository;
 
 import bangbang.gourmet.restaurant.entity.Restaurant;
 import bangbang.gourmet.restaurant.entity.RestaurantCategory;
-import io.lettuce.core.dynamic.annotation.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface RestaurantCategoryRepository extends JpaRepository<RestaurantCategory, Long> {
     // @Modifying이 있어야 '삭제'나 '수정' 쿼리가 작동합니다.
