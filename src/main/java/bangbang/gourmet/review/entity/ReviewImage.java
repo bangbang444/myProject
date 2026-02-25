895ed87ed2144d4df7e1c532d1b00660cb106ce4
bangbang444 <kim1228com@naver.com>
2026-02-25 10:22:23 +0900

    [FEAT] 리뷰-이미지 연관관계 매핑 및 최적화 쿼리 추가
    - 리뷰 이미지(ReviewImage) 엔티티 추가 및 리뷰와 1:N 매핑
    - ReviewRepository 내 Fetch Join을 이용한 리뷰-사용자-이미지 조회 최적화
    - 클라이언트 응답을 위한 ReviewSimpleResponse DTO 추가

---
+++ src/main/java/bangbang/gourmet/review/entity/ReviewImage.java
@@ -0,0 +1,20 @@
+package bangbang.gourmet.review.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class ReviewImage {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "review_id") // 식당이 아닌 리뷰 ID와 연결
+    private Review review;
+
+    private String imageUrl;
+}
