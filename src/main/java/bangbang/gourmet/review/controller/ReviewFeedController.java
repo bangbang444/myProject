48660e9a5c1e3743f827b6681a7e3e78fc046cbc
bangbang444 <kim1228com@naver.com>
2026-03-10 03:33:56 +0900

    [CHORE] 불필요한 import 제거

--- src/main/java/bangbang/gourmet/review/controller/ReviewFeedController.java
+++ src/main/java/bangbang/gourmet/review/controller/ReviewFeedController.java
@@ -7,8 +7,6 @@ import bangbang.gourmet.review.dto.FeedDetailResponse;
 import bangbang.gourmet.review.dto.ReviewFeedResponse;
 import bangbang.gourmet.review.service.ReviewFeedService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
