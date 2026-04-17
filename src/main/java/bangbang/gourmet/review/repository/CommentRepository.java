ff8432308e43bbe68fd5ebdd045c864a6f8d88d9
bangbang444 <kim1228com@naver.com>
2026-04-17 15:21:28 +0900

    [PERF] 리뷰 피드 좋아요/댓글 N+1 쿼리 배치 조회로 개선
    - ReviewLikeRepository: countByReviewIds, findLikedReviewIdsByUserIdAndReviewIds 배치 쿼리 추가
    - CommentRepository: countByReviewIds 배치 쿼리 추가

--- src/main/java/bangbang/gourmet/review/repository/CommentRepository.java
+++ src/main/java/bangbang/gourmet/review/repository/CommentRepository.java
@@ -12,9 +12,11 @@ public interface CommentRepository extends JpaRepository<Comment, Long> {
 
     long countByReview(Review review);
 
-    // TODO: fetch join
     List<Comment> findAllByReviewOrderByCreatedDate(Review review);
 
     @Query("select c from Comment c join fetch c.user where c.review = :review order by c.createdDate")
     List<Comment> findAllByReviewWithUserOrderByCreatedDate(@Param("review") Review review);
+
+    @Query("select c.review.id, count(c) from Comment c where c.review.id in :reviewIds group by c.review.id")
+    List<Object[]> countByReviewIds(@Param("reviewIds") List<Long> reviewIds);
 }
